233993ca514a5ddff4f080981cac7c3d93959d5c
roy <qwer789852@gmail.com>
2014-08-07 14:50:24 +0800

    create Posts table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140804110130) do
+ActiveRecord::Schema.define(version: 20140807064804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: true do |t|
+    t.string  "title"
+    t.string  "url"
+    t.text    "description"
+    t.integer "user_id"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
