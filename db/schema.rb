57919156e69d05bf33dd6bf3f1cdfdade35989ba
roy <qwer789852@gmail.com>
2014-08-07 14:57:13 +0800

    add timestamps to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140807064804) do
+ActiveRecord::Schema.define(version: 20140807065524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
